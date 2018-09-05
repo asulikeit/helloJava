0fd97087a9b21f01e79fc27ab26d855e01d3cb94
daniel <senticat.lee@gmail.com>
2018-09-06 07:15:31 +0900

    bfs

---
+++ src/BFS1.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS1 {
+	
+	public static void main(String[] args) {
+		GraphBfs graph = new GraphBfs(6);
+		graph.insert(1, 2);
+		graph.insert(3, 1);
+		graph.insert(2, 3);
+		graph.insert(4, 2);
+		graph.insert(3, 6);
+		graph.insert(5, 4);
+		graph.bfs(); // 1, 2, 3, 6, 4, 5, 3(x)
+	}
+}
+
+class GraphBfs {
+	private ArrayList<ArrayList<Integer>> v;
+	private boolean[] visited;
+	private Queue<Integer> queue = new LinkedList<Integer>();
+	public GraphBfs(int n) {
+		v = new ArrayList<ArrayList<Integer>>(n+1);
+		for (int i=0; i<=n; i++) {
+			v.add(new ArrayList<Integer>());
+		}
+		visited = new boolean[n+1];
+		queue.add(1);
+	}
+	
+	public void insert(int x, int y) {
+		if (x > y) {
+			int temp = x;
+			x = y;
+			y = temp;
+		}
+		v.get(x).add(y);
+	}
+
+	public void bfs() {
+		int node = queue.poll();
+		for (int now : v.get(node)) {
+			if (!visited[now]) {
+				System.out.print(now + " ");
+				queue.add(now);
+				visited[now] = true;
+				bfs();
+			}
+		}
+	}
+}
